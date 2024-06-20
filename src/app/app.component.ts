971133a60276468e88b7ae181b0b37a1191ce489
wasit-shafi <wasitshafi700@gmail.com>
2024-06-20 06:54:19 +0530

    init. dev and staging env + connected FE with BE using CORS config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dev-tools-studio';
+  tempData: any;
+
+  constructor(private httpClient: HttpClient) {
+    const url = `${environment.baseUrl}/user`;
+
+    this.httpClient.get(url).subscribe({
+      next: (response) => {
+        this.tempData = response;
+      },
+      error: (error) => {
+        console.log('error :: ', error);
+      },
+    });
+  }
 }
