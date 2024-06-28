0f9672b1c71cf1e66823099e2a5871233721c6d8
wasit-shafi <wasitshafi700@gmail.com>
2024-06-28 08:01:04 +0530

    prettier + tailwind + prettier plugin tailwind configs done

---
+++ src/app/components/index.ts
@@ -0,0 +1,3 @@
+export * from './shared/footer/footer.component';
+export * from './shared/header/header.component';
+export * from './shared/modal/modal.component';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { commonEnvironment } from './environment.common';
 
 export const environment = {
-  ...commonEnvironment,
-  production: true,
-  baseUrl: 'http://localhost:3000/api/v1',
+	...commonEnvironment,
+	production: true,
+	baseUrl: 'http://localhost:3000/api/v1',
 };
