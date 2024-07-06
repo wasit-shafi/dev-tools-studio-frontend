c0c80b54d13c26a0192802186dd107777274b37c
wasit-shafi <wasitshafi700@gmail.com>
2024-07-06 05:59:19 +0530

    added node mailer + added core views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,56 +1,54 @@
 import { Routes } from '@angular/router';
 
-import { SignupComponent, HomeComponent, SigninComponent, ForgotPasswordComponent, GuestComponent } from '@views/';
-import { SettingsComponent } from '@views/settings/settings.component';
-import { AdminHomeComponent } from './module/components/admin/admin-home/admin-home.component';
-import { PageNotFoundComponent } from '@views/page-not-found/page-not-found.component';
+import * as CoreViews from '@views/';
+import * as AdminViews from '@adminViews/';
 
 export const routes: Routes = [
 	// { path: '', redirectTo: 'home', pathMatch: 'full' },
 	{
 		path: '',
-		component: HomeComponent,
+		component: CoreViews.HomeComponent,
 		title: 'Signup - Dev Tools Studio',
 	},
 	{
 		path: 'signup',
-		component: SignupComponent,
+		component: CoreViews.SignupComponent,
 		title: 'Signup - Dev Tools Studio',
 	},
 	{
 		path: 'signin',
-		component: SigninComponent,
+		component: CoreViews.SigninComponent,
 		title: 'Signin - Dev Tools Studio',
 	},
 	{
 		path: 'forgot-password',
-		component: ForgotPasswordComponent,
+		component: CoreViews.ForgotPasswordComponent,
 		title: 'Forgot Password - Dev Tools Studio',
 	},
 	{
 		path: 'home',
-		component: HomeComponent,
+		component: CoreViews.HomeComponent,
 		title: 'Home - Dev Tools Studio',
 	},
 	{
 		path: 'guest',
-		component: GuestComponent,
+		component: CoreViews.GuestComponent,
 		title: 'Guest - Dev Tools Studio',
 	},
 	{
 		path: 'settings',
-		component: SettingsComponent,
+		component: CoreViews.SettingsComponent,
 		title: 'Settings - Dev Tools Studio',
 	},
 	// TODO: lazy load admin module/component
 	{
 		path: 'admin-home',
-		component: AdminHomeComponent,
+		component: AdminViews.AdminHomeComponent,
 		title: 'Admin Home - Dev Tools Studio',
 	},
 	{
 		path: '**',
-		component: PageNotFoundComponent,
+		component: CoreViews.PageNotFoundComponent,
 		title: 'Page Not Found',
 	},
 ];
--- src/app/views/index.ts
+++ src/app/views/index.ts
@@ -1,5 +1,7 @@
+export * from './fallbacks/page-not-found/page-not-found.component';
 export * from './forgot-password/forgot-password.component';
 export * from './guest/guest.component';
 export * from './home/home.component';
+export * from './settings/settings.component';
 export * from './signin/signin.component';
 export * from './signup/signup.component';
