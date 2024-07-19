4aada273a86d5a3f6d1f96e245919168f2e8d561
wasit-shafi <wasitshafi700@gmail.com>
2024-07-19 16:08:28 +0530

    added mock form in reset password and sign in views

--- src/app/core/views/reset-password/reset-password.component.ts
+++ src/app/core/views/reset-password/reset-password.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+import { Constants } from '@coreShared/';
+import { environment } from '@environments/';
 
 @Component({
-  selector: 'app-reset-password',
-  standalone: true,
-  imports: [],
-  templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+	selector: 'app-reset-password',
+	standalone: true,
+	imports: [FormsModule],
+	providers: [Constants],
+	templateUrl: './reset-password.component.html',
+	styleUrl: './reset-password.component.scss',
 })
 export class ResetPasswordComponent {
+	private http = inject(HttpClient);
 
+	constructor(public constants: Constants) {}
+
+	public resetPasswordFormDetails = {
+		email: '',
+		otp: '',
+	};
+
+	handleOnSubmitResetPasswordForm(event: any, signinForm: NgForm) {
+		event.preventDefault();
+
+		signinForm.reset();
+	}
 }
--- src/app/core/views/signin/signin.component.ts
+++ src/app/core/views/signin/signin.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
+import { Constants } from '@coreShared/';
+import { environment } from '@environments/';
 @Component({
 	selector: 'app-signin',
 	standalone: true,
-	imports: [],
+	imports: [FormsModule],
+	providers: [Constants],
 	templateUrl: './signin.component.html',
 	styleUrl: './signin.component.scss',
 })
-export class SigninComponent {}
+export class SigninComponent {
+	private http = inject(HttpClient);
+
+	constructor(public constants: Constants) {}
+
+	public signinFormDetails = {
+		email: '',
+		password: '',
+	};
+
+	handleOnSubmitSigninForm(event: any, signinForm: NgForm) {
+		event.preventDefault();
+
+		signinForm.reset();
+	}
+}
