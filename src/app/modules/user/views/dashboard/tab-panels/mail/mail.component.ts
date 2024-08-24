7f61083ceb002b8a7b69f1dfb2900ab1bf3063ec
wasit-shafi <wasitshafi700@gmail.com>
2024-08-24 23:03:27 +0530

    replaced automate email template form with reactive form

--- src/app/modules/user/views/dashboard/tab-panels/mail/mail.component.ts
+++ src/app/modules/user/views/dashboard/tab-panels/mail/mail.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 import { Constants } from '@coreShared/';
@@ -8,8 +8,8 @@ import { environment } from '@environments/';
 @Component({
 	selector: 'app-mail',
 	standalone: true,
-	imports: [FormsModule],
-	providers: [Constants, NgForm],
+	imports: [ReactiveFormsModule],
+	providers: [Constants, FormBuilder],
 	templateUrl: './mail.component.html',
 	styleUrl: './mail.component.scss',
 })
@@ -19,21 +19,22 @@ export class MailComponent {
 
 	private http = inject(HttpClient);
 	private constants = inject(Constants);
+	private formBuilder = inject(FormBuilder);
 
-	public sendEmailFormModel = {
-		to: '',
-		subject: '',
-		salutation: '',
-		body: '',
-		closing: '',
-		signature: '',
-	};
+	public mailForm = this.formBuilder.group({
+		to: ['', Validators.required],
+		subject: ['', Validators.required],
+		salutation: ['', Validators.required],
+		body: ['', Validators.required],
+		closing: ['', Validators.required],
+		signature: ['', Validators.required],
+		attachments: this.formBuilder.array([this.formBuilder.control('')]),
+	});
 
-	handleOnSubmitSendEmailForm(event: SubmitEvent, form: NgForm) {
-		event.preventDefault();
+	handleOnSubmitSendEmailForm() {
 		const url = `${environment.baseUrl}${this.constants.API._V1}/mail/send`;
 
-		this.http.post(url, { ...this.sendEmailFormModel }).subscribe({
+		this.http.post(url, { ...this.mailForm.value }).subscribe({
 			next: (response) => {
 				console.log('response ::', response);
 			},
