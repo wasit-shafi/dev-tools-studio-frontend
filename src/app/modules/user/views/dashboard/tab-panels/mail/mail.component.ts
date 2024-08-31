a32a36aaea4e82cdec209e2ea4b25a5bf8bde145
wasit-shafi <wasitshafi700@gmail.com>
2024-08-31 06:36:49 +0530

    init mail changes

--- src/app/modules/user/views/dashboard/tab-panels/mail/mail.component.ts
+++ src/app/modules/user/views/dashboard/tab-panels/mail/mail.component.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Constants } from '@coreShared/';
 import { environment } from '@environments/';
+import { ToastService } from '@coreServices/index';
 
 @Component({
 	selector: 'app-mail',
@@ -21,9 +22,11 @@ export class MailComponent {
 	private http = inject(HttpClient);
 	private constants = inject(Constants);
 	private formBuilder = inject(FormBuilder);
+	private toastService = inject(ToastService);
 
 	public mailForm = this.formBuilder.nonNullable.group({
-		to: ['', [Validators.required, Validators.email]],
+		dateTimeLocal: ['', [Validators.required]],
+		to: ['wasitshafi786@gmail.com', [Validators.required, Validators.email]],
 		subject: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
 		salutation: ['', [Validators.required]],
 		body: ['', [Validators.required]],
@@ -40,11 +43,19 @@ export class MailComponent {
 		const url = `${environment.baseUrl}${this.constants.API._V1}/mail/send`;
 
 		this.http.post(url, { ...this.mailForm.value }).subscribe({
-			next: (response) => {
-				console.log('response ::', response);
+			next: (response: any) => {
+				// console.log('response ::', response);
+				this.toastService.enqueueToastNotification({
+					message: response.message,
+					type: this.constants.ALERT_TYPE.SUCCESS,
+				});
 			},
 			error: (error) => {
-				console.log('error :: ', error);
+				// console.log('error :: ', error);
+				this.toastService.enqueueToastNotification({
+					message: error.error.message || error.message,
+					type: this.constants.ALERT_TYPE.ERROR,
+				});
 			},
 		});
 	}
