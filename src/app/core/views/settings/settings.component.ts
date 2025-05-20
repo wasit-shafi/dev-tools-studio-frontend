29ad7dae1dc7c38c54eff204fc6a06e8b7b584dc
wasit-shafi <wasitshafi700@gmail.com>
2025-05-21 01:20:15 +0530

    added account info in settings

--- src/app/core/views/settings/settings.component.ts
+++ src/app/core/views/settings/settings.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import {
     AddCredentialComponent, AddEmailTemplateComponent, ListCredentialComponent, ListEmailTemplateComponent
 } from '@coreComponents/';
+import { authFeature } from '@coreStore/';
+import { Store } from '@ngrx/store';
 
 @Component({
 	selector: 'dts-settings',
-	imports: [AddCredentialComponent, ListCredentialComponent, AddEmailTemplateComponent, ListEmailTemplateComponent],
+	imports: [
+		AddCredentialComponent,
+		ListCredentialComponent,
+		AddEmailTemplateComponent,
+		ListEmailTemplateComponent,
+		AsyncPipe,
+	],
 	templateUrl: './settings.component.html',
 	styleUrl: './settings.component.scss',
 })
-export class SettingsComponent {}
+export class SettingsComponent {
+	private readonly store = inject(Store);
+	protected currentUser$ = this.store.select(authFeature.selectCurrentUser);
+}
